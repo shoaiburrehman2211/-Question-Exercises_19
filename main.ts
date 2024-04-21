3f4ac2b85ab8b282a9001e4d934b0c3202082878
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 03:36:09 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,15 @@
+/*
+Working with one of the programs from Exercises 14 through 18, print a message indicating 
+the number of people you are inviting to dinner.
+*/
+
+let myFriends: string[] = ["Zohaib", "Ayaz", "Faisal", "Junaid", "Rameez"];
+
+console.log(`I am inviting ${myFriends.length} friends to my dinner which are following\n`);
+
+for(let i = 0; i< myFriends.length; i++){
+    console.log(`${i + 1}. ${myFriends[i]} `)
+}
+
+
+
